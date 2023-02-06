8043932ca587036e3f2c6c649fdbc9201a8e7a7f
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-06 16:23:36 -0300

    Make extraction script executable for tests.
    (cherry picked from commit 40fa3b42d0ab903ba759e6a44928bb4c746f2a7c)

--- scripts/Extractor.py
+++ scripts/Extractor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import psycopg2
 import yaml
@@ -61,7 +63,7 @@ with psycopg2.connect(credentials) as conn:
             query = f"SELECT * FROM {table_name}"
             table = pd.read_sql(query, conn)
             output_name = f'''
-            /data/postgres/{execution_date}/{table_name}.csv
+            /data/postgres/{extraction_date}/{table_name}.csv
             '''
             table.to_csv(output_name)
 
@@ -71,6 +73,6 @@ conn.close() # contexts do not close the connection, only commit or
 # extract data from the provided csv file
 order_details = pd.read_csv(ORDER_DETAILS_PATH)
 order_details_output_path = f'''
-/data/csv/{execution_date}/order_details.csv
+/data/csv/{extraction_date}/order_details.csv
 '''
 order_details.to_csv(order_details_output_path)
